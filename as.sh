8fe70cbc616c9f9f73bd982d7fd06150b26bb1dc
Orbit Byrd <orbitbyrd@gmail.com>
2021-11-21 13:19:48 -0800

    perm

--- as.sh
+++ as.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-go build -o /home/ec2-user/simpleskiresort/bin/main /home/ec2-user/simpleskiresort/main.go 
+sudo go build -o /home/ec2-user/simpleskiresort/bin/main /home/ec2-user/simpleskiresort/main.go 
 chmod a+x /home/ec2-user/simpleskiresort/bin/main
 /home/ec2-user/simpleskiresort/bin/main
