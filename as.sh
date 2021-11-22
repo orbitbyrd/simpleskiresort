b687b03dabfd9fa60edff80ea5ef61da0b5676df
Orbit Byrd <orbitbyrd@gmail.com>
2021-11-21 18:38:45 -0800

    start stop

--- as.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/simpleskiresort/
-sudo go build -o bin/main main.go 
-sudo chmod a+x /home/ec2-user/simpleskiresort/bin/main
-/home/ec2-user/simpleskiresort/bin/main > /dev/null 2> /dev/null < /dev/null &
