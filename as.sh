325f4cabc3b70ce2565b130b8c94fdbc311d1372
Orbit Byrd <orbitbyrd@gmail.com>
2021-11-21 17:53:11 -0800

    a

--- as.sh
+++ as.sh
@@ -2,4 +2,4 @@
 cd /home/ec2-user/simpleskiresort/
 sudo go build -o bin/main main.go 
 sudo chmod a+x /home/ec2-user/simpleskiresort/bin/main
-/home/ec2-user/simpleskiresort/bin/main > /dev/null 2> /dev/null < /dev/null &
+/home/ec2-user/simpleskiresort/bin/main > /dev/null &
