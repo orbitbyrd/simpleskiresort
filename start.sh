28f26835c1345a690d31744b6640aeb15bdd0508
Orbit Byrd <orbitbyrd@gmail.com>
2022-01-09 13:29:13 -0800

    g

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 cd /home/ec2-user/simpleskiresort/
 sudo  make run 
 sudo chmod a+x /home/ec2-user/simpleskiresort/bin/simpleskiresort
-/home/ec2-user/simpleskiresort/bin/simpleskiresort > /dev/null 2> /dev/null < /dev/null &
+/home/ec2-user/simpleskiresort/bin/simpleskiresort
