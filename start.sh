cdeb803f706d6eebe19dd2f53a39704244f019c2
Orbit Byrd <orbitbyrd@gmail.com>
2022-01-09 12:18:00 -0800

    dt

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ${SSR_HOME}
-sudo  make run 
+sudo  ${SSR_HOME}/make run 
 sudo chmod a+x ${SSR_HOME}/bin/simpleskiresort
 ${SSR_HOME}/bin/simpleskiresort > /dev/null 2> /dev/null < /dev/null &
