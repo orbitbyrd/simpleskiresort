e3054023519f8b0b153a50c110ab1e3e3b50b886
Orbit Byrd <orbitbyrd@gmail.com>
2022-01-09 12:43:36 -0800

    rem env

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ${SSR_HOME}
-sudo  ${SSR_HOME}/make run 
-sudo chmod a+x ${SSR_HOME}/bin/simpleskiresort
-${SSR_HOME}/bin/simpleskiresort > /dev/null 2> /dev/null < /dev/null &
+cd /home/ec2-user/simpleskiresort/
+sudo  make run 
+sudo chmod a+x /home/ec2-user/simpleskiresort/bin/simpleskiresort
+/home/ec2-user/simpleskiresort/bin/simpleskiresort > /dev/null 2> /dev/null < /dev/null &
