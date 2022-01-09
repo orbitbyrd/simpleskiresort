3c8c8b029a80e8164891093650f39986205be5ad
Orbit Byrd <orbitbyrd@gmail.com>
2022-01-09 13:49:28 -0800

    h

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+export SSR_HOME="/home/ec2-user/simpleskiresort"
 cd /home/ec2-user/simpleskiresort/
 sudo  make run 
 sudo chmod a+x /home/ec2-user/simpleskiresort/bin/simpleskiresort
-/home/ec2-user/simpleskiresort/bin/simpleskiresort
+/home/ec2-user/simpleskiresort/bin/simpleskiresort > /dev/null 2> /dev/null < /dev/null &
